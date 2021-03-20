579be4c85fd07bdeadab990e69f21d101efe114a
Saad-001 <saadafnan21@gmail.com>
2021-03-20 14:40:23 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
 } from "react-router-dom";
 import CreateAccount from "./Components/CreateAccount/CreateAccount";
 import Home from "./Components/Home/Home";
